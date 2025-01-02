ca8dc2363399538a5c47fad70a0edd14d16a04d2
dorname <lgqfighting@163.com>
2025-01-03 07:57:15 +0800

    更新二叉树实现

--- src/tree/binary_tree.rs
+++ src/tree/binary_tree.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::fmt;
 use std::rc::Rc;
 /**
@@ -40,7 +41,80 @@ impl TreeNode {
         self.right = Some(right);
     }
 }
+/**
+ * 层序遍历
+ */
+pub fn level_order(root: &Rc<RefCell<TreeNode>>) -> Vec<i32> {
+    let mut queue = VecDeque::new();
+    queue.push_back(root.clone());
+    let mut result = Vec::new();
+    while let Some(node) = queue.pop_front() {
+        result.push(node.borrow().val);
+        if let Some(left) = node.borrow().left.clone() {
+            // 左子树入队
+            queue.push_back(left);
+        }
+        if let Some(right) = node.borrow().right.clone() {
+            // 右子树入队
+            queue.push_back(right);
+        }
+    }
+    result
+}
 
+/**
+ * 前序优先遍历
+ */
+fn pre_order(root: Option<&Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = Vec::new();
+    fn dfs(root: Option<&Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        // 根->左->右
+        if let Some(node) = root {
+            let node = node.borrow();
+            res.push(node.val);
+            dfs(node.left.as_ref(), res);
+            dfs(node.right.as_ref(), res);
+        }
+    }
+    dfs(root, &mut result);
+    result
+}
+
+/**
+ * 中序遍历
+ */
+fn in_order(root: Option<&Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = Vec::new();
+    fn dfs(root: Option<&Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        // 左->根->右
+        if let Some(node) = root {
+            let node = node.borrow();
+            dfs(node.left.as_ref(), res);
+            res.push(node.val);
+            dfs(node.right.as_ref(), res);
+        }
+    }
+    dfs(root, &mut result);
+    result
+}
+
+/**
+ * 后序遍历
+ */
+fn post_order(root: Option<&Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = Vec::new();
+    fn dfs(root: Option<&Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        // 左->右->根
+        if let Some(node) = root {
+            let node = node.borrow();
+            dfs(node.left.as_ref(), res);
+            dfs(node.right.as_ref(), res);
+            res.push(node.val);
+        }
+    }
+    dfs(root, &mut result);
+    result
+}
 mod tests {
     use super::*;
     #[test]
@@ -59,5 +133,53 @@ mod tests {
         // n2.borrow_mut().left = Some(n3);
         // n2.borrow_mut().right = Some(n4);
         println!("{:?}", root);
+    }
+
+    #[test]
+    fn level_order_test() {
+        let root = TreeNode::new(1);
+        let n1: Rc<RefCell<TreeNode>> = TreeNode::new(2);
+        let n2 = TreeNode::new(3);
+        let n3 = TreeNode::new(4);
+        let n4 = TreeNode::new(5);
+        root.borrow_mut().insert(n1, n2.clone());
+        n2.borrow_mut().insert(n3, n4);
+        assert_eq!(level_order(&root), vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn pre_order_test() {
+        let root = TreeNode::new(1);
+        let n1: Rc<RefCell<TreeNode>> = TreeNode::new(2);
+        let n2 = TreeNode::new(3);
+        let n3 = TreeNode::new(4);
+        let n4 = TreeNode::new(5);
+        root.borrow_mut().insert(n1, n2.clone());
+        n2.borrow_mut().insert(n3, n4);
+        assert_eq!(pre_order(Some(&root)), vec![1, 2, 4, 5, 3]);
+    }
+
+    #[test]
+    fn in_order_test() {
+        let root = TreeNode::new(1);
+        let n1: Rc<RefCell<TreeNode>> = TreeNode::new(2);
+        let n2 = TreeNode::new(3);
+        let n3 = TreeNode::new(4);
+        let n4 = TreeNode::new(5);
+        root.borrow_mut().insert(n1, n2.clone());
+        n2.borrow_mut().insert(n3, n4);
+        assert_eq!(in_order(Some(&root)), vec![2, 1, 4, 3, 5]);
+    }
+
+    #[test]
+    fn post_order_test() {
+        let root: Rc<RefCell<TreeNode>> = TreeNode::new(1);
+        let n1: Rc<RefCell<TreeNode>> = TreeNode::new(2);
+        let n2 = TreeNode::new(3);
+        let n3 = TreeNode::new(4);
+        let n4 = TreeNode::new(5);
+        root.borrow_mut().insert(n1, n2.clone());
+        n2.borrow_mut().insert(n3, n4);
+        assert_eq!(post_order(Some(&root)), vec![2, 4, 5, 3, 1]);
     }
 }
