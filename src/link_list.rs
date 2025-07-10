9391521a763c08bd9efaf6285fb8a6734a6be807
dorname <lgqfighting@163.com>
2025-07-10 18:30:49 +0800

    更新单链表实现【todo】需要验证一下pop_back函数

--- src/link_list.rs
+++ src/link_list.rs
@@ -5,7 +5,8 @@ type Node<T> = Option<Box<LinkListNode<T>>>;
 struct LinkListNode<T> {
     // 存储任意类型的数据
     data: T,
-    // 指向下一个节点的 Option<Box<Node<T>>>，使用 Box 进行堆内存分配，Option 来表示节点可能为空。
+    // 指向下一个节点的 Option<Box<Node<T>>>，
+    // 使用 Box 进行堆内存分配，Option 来表示节点可能为空。
     next: Node<T>,
 }
 
@@ -14,11 +15,16 @@ struct LinkListNode<T> {
 struct LinkList<T> {
     // 链表的头节点
     head: Node<T>,
+    // 链表长度
+    lenght: usize,
 }
 impl<T> LinkList<T> {
     // 创建一个空的链表
     fn new() -> Self {
-        LinkList { head: None }
+        LinkList {
+            head: None,
+            lenght: 0,
+        }
     }
 
     // 在链表头部插入一个节点
@@ -53,8 +59,8 @@ impl<T> LinkList<T> {
     }
 
     // 弹出节点
-    fn pop(&mut self) -> Option<T> {
-        let mut head = self.head.take();
+    fn pop_front(&mut self) -> Option<T> {
+        let head = self.head.take();
         match head {
             Some(node) => {
                 // Extract the data from the node
@@ -64,6 +70,87 @@ impl<T> LinkList<T> {
             }
             None => None,
         }
+    }
+
+    // 弹出链表尾部节点
+    fn pop_back(&mut self) -> Option<T> {
+        let len = self.len();
+        let mut current = &mut self.head;
+        for _ in 0..len - 1 {
+            if let Some(node) = current {
+                current = &mut node.next;
+            }
+        }
+        let tail = current.take();
+        match tail {
+            Some(node) => Some(node.data),
+            None => None,
+        }
+    }
+
+    // 插入节点
+    fn insert(&mut self, data: T, index: usize) {
+        // 1、判断index 是否超过链表长度了
+        if index > self.lenght {
+            panic!("Overflow: index > LinkList lenght");
+        }
+
+        let new_node = Box::new(LinkListNode { data, next: None });
+
+        // 如果是插入到头部
+        if index == 0 {
+            let mut new_node = new_node;
+            new_node.next = self.head.take();
+            self.head = Some(new_node);
+            self.lenght += 1;
+            return;
+        }
+
+        // 找到插入位置的前一个节点
+        let mut current = &mut self.head;
+        for _ in 0..index - 1 {
+            if let Some(node) = current {
+                current = &mut node.next;
+            }
+        }
+
+        // 执行插入操作
+        if let Some(node) = current {
+            let mut new_node = new_node;
+            new_node.next = node.next.take();
+            node.next = Some(new_node);
+            self.lenght += 1;
+        }
+    }
+
+    // 删除
+    fn remove_at(&mut self, index: usize) {
+        if index == 0 {
+            self.pop_front();
+            return;
+        }
+
+        // 找到删除位置的前一个节点
+        let mut current = &mut self.head;
+        for _ in 0..index - 1 {
+            if let Some(node) = current {
+                current = &mut node.next;
+            }
+        }
+
+        //执行删除操作
+        if let Some(node) = current {
+            let mut remove_node = node.next.take();
+            if let Some(mut re_node) = remove_node {
+                node.next = re_node.next.take();
+            }
+            self.lenght -= 1;
+        }
+    }
+
+    // 添加一个辅助方法来获取链表长度
+    fn len(&self) -> usize {
+        self.lenght
     }
 }
 
@@ -85,7 +172,61 @@ mod link_list_tests {
         list.push(1);
         list.push(2);
         list.push(3);
-        list.pop();
+        list.pop_front();
         println!("{:?}", list);
+    }
+
+    #[test]
+    fn insert_test() {
+        let mut list = LinkList::new();
+
+        // 测试在空链表中插入
+        list.insert(1, 0);
+        assert_eq!(list.len(), 1);
+
+        // 测试在头部插入
+        list.insert(2, 0);
+        assert_eq!(list.len(), 2);
+
+        // 测试在中间插入
+        list.insert(3, 1);
+        assert_eq!(list.len(), 3);
+
+        // 测试在尾部插入
+        list.insert(4, 3);
+        assert_eq!(list.len(), 4);
+
+        // 验证插入后的弹出顺序
+        assert_eq!(list.pop_front(), Some(2)); // 头部
+        assert_eq!(list.pop_front(), Some(3)); // 第二个
+        assert_eq!(list.pop_front(), Some(1)); // 第三个
+        assert_eq!(list.pop_front(), Some(4)); // 尾部
+        assert_eq!(list.pop_front(), None); // 空链表
+    }
+
+    #[test]
+    fn remove_at_test() {
+        let mut list = LinkList::new();
+
+        // 测试在空链表中插入
+        list.insert(1, 0);
+        assert_eq!(list.len(), 1);
+
+        // 测试在头部插入
+        list.insert(2, 0);
+        assert_eq!(list.len(), 2);
+
+        // 测试在中间插入
+        list.insert(3, 1);
+        assert_eq!(list.len(), 3);
+
+        // 测试在尾部插入
+        list.insert(4, 3);
+        assert_eq!(list.len(), 4);
+
+        println!("{:?}", list);
+        list.remove_at(0);
+        println!("{:?}", list);
+        assert_eq!(list.pop_back(), Some(1))
     }
 }
